71f582151f7fc46684b2a247740a821d06ab289c
Oluwaseun241 <tanimolaoluwaseun70@gmail.com>
2024-09-23 11:39:41 +0100

    wip

---
+++ frontend-api/app/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
+
+Base = declarative_base()
+
+class User(Base):
+
+class Book(Base):
+
+class Borrow(Base):
